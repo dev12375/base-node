29baa3c2751170626905811efd38ff81937e6674
mark <mark@gmail.com>
2024-04-16 22:10:21 +0800

    feat: update script

--- 1-apt-change-zsh.sh
+++ 1-apt-change-zsh.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+NEW_PORT=10086
+PUB_KEY="pubkey"
 install_dep(){
     apt-get update
     apt-get install -y git gcc g++ zsh aria2 make net-tools tmux jq pigz
@@ -9,5 +11,19 @@ change_zsh(){
     chsh -s /usr/bin/zsh
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
 }
+change_ssh(){
+    cp /etc/ssh/sshd_config /etc/ssh/sshd_config_backup
+
+    # 修改 SSH 配置文件中的端口号
+    sed -i "s/#Port 22/Port $NEW_PORT/" /etc/ssh/sshd_config
+
+    # 启动 SSH 服务
+    systemctl start ssh
+
+    cat $PUB_KEY >> ~/.ssh/authorized_keys
+
+}
+
 install_dep
+change_ssh
 change_zsh

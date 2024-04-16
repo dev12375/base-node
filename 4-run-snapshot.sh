84b312ef6ed328739b3394873e6a4dc32a9ed913
mark <mark@gmail.com>
2024-04-16 21:05:33 +0800

    feat: update script

--- 4-run-snapshot.sh
+++ 4-run-snapshot.sh
@@ -1,10 +1,12 @@
 
 snap_shot_base() {
-    mkdir -p $HOME/base-chain-node/data
-    cd $HOME/base-chain-node/
+    path=$HOME/base-chain-node
+    mkdir -p $path/data
+    cd $path
     filename=$(curl -s https://mainnet-full-snapshots.base.org/latest)
-    aria2c -o "$filename" -x 4 -s 12 "https://mainnet-full-snapshots.base.org/$filename" && tar -xzvf "$filename" -C $HOME/base-chain-node/data
-    cd $HOME/base-chain-node/base-node
+    aria2c -o "$filename" -x 4 -s 12 "https://mainnet-full-snapshots.base.org/$filename" 
+    tar -xvf "$filename"  -C $path/data --use-compress-program=pigz
+    cd $path/base-node
     docker compose up --build
 }
 
